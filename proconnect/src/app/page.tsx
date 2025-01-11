b90d7898703b88d9946d68c7425a58872dc308da
billiezinha <joaofernandesrodrigues78@gmail.com>
2025-01-11 17:23:02 -0300

    tela inicial

--- proconnect/src/app/page.tsx
+++ proconnect/src/app/page.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
-import  styles  from "../styles/page.module.css";
+import styles from "./page.module.css";
+
 
 export default function Home() {
   return (
-    <div className="{styles.body}">
-      <h1 className="{styles.titulo}">ProConnect</h1>
-      <h2 className="{styles.texto}">Simplifique a busca porserviços e profissionais com a plataforma mais direta e transparente do mercado</h2>
-
+    <div className={styles.body}>
+      <h1 className={styles.titulo}>CONECTANDO</h1>
+      <h1 className={styles.titulo2}>PROFISSIONAIS E CLIENTES</h1>
+      <h2 className={styles.texto}>Simplifique a busca porserviços e profissionais com a plataforma mais direta e transparente do mercado</h2>
+    
+      <div>
+    <button className={styles.butao}>ANUNCIE SEU SERVIÇO</button>
+    <button className={styles.butao}>ENCONTRE PROFISSIONAIS</button>
+      </div>
     </div>
   );
 }
