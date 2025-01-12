6740e5f762fec9650fbde47dc037ba38e7965dd1
Emyllebsousa <emyllebeatrizdesousa@gmail.com>
2025-01-11 21:17:52 -0300

    Recursos

--- proconnect/src/app/page.tsx
+++ proconnect/src/app/page.tsx
@@ -1,18 +1,20 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-
+import Recursos from "./Recursos/pages";
 
 export default function Home() {
   return (
     <div className={styles.body}>
       <h1 className={styles.titulo}>CONECTANDO</h1>
       <h1 className={styles.titulo2}>PROFISSIONAIS E CLIENTES</h1>
-      <h2 className={styles.texto}>Simplifique a busca porserviços e profissionais com a plataforma mais direta e transparente do mercado</h2>
+      <h2 className={styles.texto}>Simplifique a busca por serviços e profissionais com a plataforma mais direta e transparente do mercado</h2>
     
       <div>
     <button className={styles.butao}>ANUNCIE SEU SERVIÇO</button>
     <button className={styles.butao}>ENCONTRE PROFISSIONAIS</button>
       </div>
+ <div>
+ <Recursos/>
+</div>
     </div>
   );
 }
