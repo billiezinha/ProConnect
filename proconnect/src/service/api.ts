37ebd595bc9381acf2948bfb59d3a88aca9d3402
billiezinha <joaofernandesrodrigues78@gmail.com>
2025-07-15 17:20:04 -0300

    Modal corrigido

--- proconnect/src/service/api.ts
+++ proconnect/src/service/api.ts
@@ -1,10 +1,6 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:3333"
-    : "https://proconnectapi.onrender.com";
-
+const baseURL = "https://proconnectapi.onrender.com";
 
 const api = axios.create({
   baseURL,
