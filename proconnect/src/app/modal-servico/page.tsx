0c2074a5b07eaedfab96f569727228bc40fa297f
billiezinha <joaofernandesrodrigues78@gmail.com>
2025-07-15 16:53:52 -0300

    Envolvendo o Modal na tag Suspense

--- proconnect/src/app/modal-servico/page.tsx
+++ proconnect/src/app/modal-servico/page.tsx
@@ -2,10 +2,15 @@
 
 import { useSearchParams } from "next/navigation";
 import Modal from "./Modal";
+import { Suspense } from "react";
 
 export default function ModalServicoPage() {
   const searchParams = useSearchParams();
   const id = Number(searchParams.get("id")); 
 
-  return <Modal id={id} onClose={() => {}} />;
+  return (
+    <Suspense>
+      <Modal id={id} onClose={() => {}} />;
+    </Suspense>
+  ) 
 }
