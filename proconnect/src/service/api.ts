94ef2cf3f4d860ded37ad8c3473913dab16f0372
leticiasbarros23 <leticiabarrosbferreira@gmail.com>
2025-07-17 19:12:34 -0300

    ajuste da api

--- proconnect/src/service/api.ts
+++ proconnect/src/service/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://proconnectapi.onrender.com";
+const baseURL = 'https://proconnectapi.onrender.com'; // ✅ produção
 
 const api = axios.create({
   baseURL,
@@ -9,11 +9,40 @@ const api = axios.create({
   },
 });
 
-
 api.interceptors.request.use((config) => {
   const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
   if (token) config.headers!["Authorization"] = `Bearer ${token}`;
   return config;
 });
 
-export default api;
+export default api;
+
+
+
+
+
+
+
+
+
+// import axios from "axios";
+
+// const baseURL = 'http://localhost:3333'
+
+// // "https://proconnectapi.onrender.com";
+
+// const api = axios.create({
+//   baseURL,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+
+// api.interceptors.request.use((config) => {
+//   const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
+//   if (token) config.headers!["Authorization"] = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default api;
